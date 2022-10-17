52fc7c0e15f515c1125eca5b40fa45b106d962cb
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-10-17 16:25:08 -0300

    Terminado os cards dos programas

--- ladingPage-gym/src/components/Programs/Programs.tsx
+++ ladingPage-gym/src/components/Programs/Programs.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import "./Programs.css"
+import React from "react";
+import programs from "../../data/programs";
+import "./Programs.css";
+import RightArrow from "../../assets/rightArrow.png"
+
 
 const Programs = () => {
   return (
-    <div className='Programs' id='programs'>
+    <div className="Programs" id="programs">
       {/* header programs */}
-      <div className='programs-header'>
-        <span className='stroke-text'>EXPLORE NOSSOS </span>
+      <div className="programs-header">
+        <span className="stroke-text">EXPLORE NOSSOS </span>
         <span>PROGRAMAS </span>
-        <span className='stroke-text'>PARA MOLDAR VOCÊ</span>
+        <span className="stroke-text">PARA MOLDAR VOCÊ</span>
+      </div>
+
+      <div className="program-categories">
+        {programs.map((program)=>(
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Entrar </span>
+              <span><img src={RightArrow} alt="" /></span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Programs
+export default Programs;
