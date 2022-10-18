50005f0787d8214d7a44f49cc69fed2ecde3b47a
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-10-18 15:52:36 -0300

    Adicionado motion em alguns elementos

--- ladingPage-gym/src/components/Hero.tsx
+++ ladingPage-gym/src/components/Hero.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header/Header";
 import "./Hero.css";
-import hero_image from"../assets/hero_image.png"
-import hero_image_back from "../assets/hero_image_back.png"
-import Heart from "../assets/heart.png"
-import calories from "../assets/calories.png"
+import hero_image from "../assets/hero_image.png";
+import hero_image_back from "../assets/hero_image_back.png";
+import Heart from "../assets/heart.png";
+import calories from "../assets/calories.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
   return (
     <div className="hero">
       <div className="blur hero-blur"></div>
@@ -14,7 +16,11 @@ const Hero = () => {
         <Header />
         {/* anuncio da melhor academia */}
         <div className="the-best-ad">
-          <div></div>
+          <motion.div
+            initial={{ left: "180px" }}
+            whileInView={{ left: "9px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
           <span>A melhor academia da cidade</span>
         </div>
         {/* título principal */}
@@ -51,28 +57,41 @@ const Hero = () => {
         <div className="buttons">
           <button className="btn">Iniciar</button>
           <button className="btn">Saiba mais</button>
-
         </div>
       </div>
       <div className="right-hero">
         <button className="btn">ENTRAR</button>
-
-        <div className="heart-rate">
+        {/* adicionado motion */}
+        <motion.div
+          initial={{ right: "-1rem" }}
+          whileInView={{ right: "4rem" }}
+          transition={transition}
+          className="heart-rate"
+        >
           <img src={Heart} alt="" />
           <span>Frequência cardíaca </span>
           <span>116 Bpm</span>
-        </div>
+        </motion.div>
         {/* imagens */}
-        <img src={hero_image} alt="" className="hero-image"/>
-        <img src={hero_image_back} alt="" className="hero-image-back" />
+        <img src={hero_image} alt="" className="hero-image" />
+        <motion.img
+        initial={{right:'11rem'}}
+        whileInView={{ right: "24rem" }}
+        transition={transition}
+        src={hero_image_back} alt="" className="hero-image-back" />
         {/* îmagen calorias */}
-        <div className="calories">
-          <img src={calories} alt=""/>
+        <motion.div
+          initial={{ right: "40rem" }}
+          whileInView={{ right: "31rem" }}
+          transition={transition}
+          className="calories"
+        >
+          <img src={calories} alt="" />
           <div>
             <span>Queima Calórica </span>
             <span>220 kcal</span>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- ladingPage-gym/src/components/Testimonials/Testimonials.tsx
+++ ladingPage-gym/src/components/Testimonials/Testimonials.tsx
@@ -3,8 +3,10 @@ import { testimonialsData } from "../../data/testimonials";
 import leftArrow from "../../assets/leftArrow.png";
 import rightArrow from "../../assets/rightArrow.png";
 import "./Testimonials.css";
+import { motion } from "framer-motion";
 
 const Testimonials = () => {
+  const transition = { type: "spring", duration: 3 };
   const [selected, setSelected] = useState(0);
   const tLenght = testimonialsData.length;
 
@@ -14,7 +16,14 @@ const Testimonials = () => {
         <span>Depoimentos </span>
         <span className="stroke-text">O que eles </span>
         <span>pensam sobre nós</span>
-        <span>{testimonialsData[selected].review}</span>
+        <motion.span
+          key={selected}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={transition}
+        
+        >{testimonialsData[selected].review}</motion.span>
         <span>
           <span style={{ color: "var(--orange)" }}>
             {testimonialsData[selected].name}
@@ -23,9 +32,23 @@ const Testimonials = () => {
         </span>
       </div>
       <div className="right-t">
-        <div></div>
-        <div></div>
-        <img src={testimonialsData[selected].image} alt="" />
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ ...transition, duration: 2 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ ...transition, duration: 2 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        ></motion.div>
+        <motion.img
+          key={selected}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={transition}
+        src={testimonialsData[selected].image} alt="" />
         <div className="arrows">
           <img
             onClick={() => {
