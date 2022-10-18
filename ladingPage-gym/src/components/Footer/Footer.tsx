3ccab86726a64627350b14aa65eb91e876caac59
Chrystian Raony Perazzoli <chrystian.raony@gmail.com>
2022-10-18 14:33:23 -0300

    Criado Footer e adicionado social links

--- ladingPage-gym/src/components/Footer/Footer.tsx
+++ ladingPage-gym/src/components/Footer/Footer.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import "./Footer.css"
-import Github from "../../assets/github.png"
-import Instagram from "../../assets/instagram.png"
-import Linkedin from "../../assets/linkedin.png"
-import Logo from "../../assets/logo.png"
+import React from "react";
+import "./Footer.css";
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import Linkedin from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
-    <div className='Footer-container'>
+    <div className="Footer-container">
       <hr />
-      <div className='footer'>
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={Linkedin} alt="" />
+        </div>
+        <div className="logo-f">
+          <img src={Logo} alt="" />
+        </div>
+          <p>Todos os direitos reservados &copy;</p> 
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
